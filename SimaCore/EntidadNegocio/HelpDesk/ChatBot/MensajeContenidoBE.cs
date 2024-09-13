32ea7c6ab3386aaae81a1dbc31eb66d41abb8e87
erosalesPRY <ejra2008@gmail.com>
2024-09-13 14:52:02 -0500

    Add files via upload

---
+++ SimaCore/EntidadNegocio/HelpDesk/ChatBot/MensajeContenidoBE.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadNegocio.HelpDesk.ChatBot
+{
+    public class MensajeContenidoBE:BaseBE
+    {
+        public int IdMiembro{  get; set; }
+        public string Texto { get; set; }
+
+        public int  IdContactoOrigen { get; set; }
+        public int IdContactoDestino { get; set; }
+
+        public string JSonBE { get; set; }
+    }
+}
